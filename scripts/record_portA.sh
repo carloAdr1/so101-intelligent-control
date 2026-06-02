f9b08f57cb64d17a586e801fb4e4f15305b53d16
Carlos Adriel <A00839986@tec.mx>
2026-06-02 11:34:00 -0600

    add portA and portB scripts for swapped leader/follower ports

--- scripts/record_portA.sh
+++ scripts/record_portA.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# Usage: bash scripts/record_il.sh <situation> <num_episodes>
-# Situations: both_bw | only_black | only_white
-SITUATION=${1:-both_bw}
+# Port config A: follower=ttyACM0, leader=ttyACM1
+SITUATION=${1:-only_black}
 NUM_EPISODES=${2:-100}
 sudo chmod 666 /dev/video0 /dev/video2
 lerobot-record \
+  --resume=true \
   --robot.type=so101_follower \
   --robot.port=/dev/ttyACM0 \
   --robot.cameras='{"front":{"type":"opencv","index_or_path":0,"width":640,"height":480,"fps":30},"side":{"type":"opencv","index_or_path":2,"width":640,"height":480,"fps":30}}' \
@@ -15,7 +15,7 @@ lerobot-record \
   --dataset.single_task="state based cable sorting: red yellow green targets, black white distractors" \
   --dataset.num_episodes=${NUM_EPISODES} \
   --dataset.episode_time_s=30 \
-  --dataset.reset_time_s=10 \
+  --dataset.reset_time_s=15 \
   --dataset.fps=30 \
   --dataset.push_to_hub=true \
   --play_sounds=false
--- scripts/record_portB.sh
+++ scripts/record_portB.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-# Usage: bash scripts/record_il.sh <situation> <num_episodes>
-# Situations: both_bw | only_black | only_white
-SITUATION=${1:-both_bw}
+# Port config B: follower=ttyACM1, leader=ttyACM0
+SITUATION=${1:-only_black}
 NUM_EPISODES=${2:-100}
 sudo chmod 666 /dev/video0 /dev/video2
 lerobot-record \
+  --resume=true \
   --robot.type=so101_follower \
-  --robot.port=/dev/ttyACM0 \
+  --robot.port=/dev/ttyACM1 \
   --robot.cameras='{"front":{"type":"opencv","index_or_path":0,"width":640,"height":480,"fps":30},"side":{"type":"opencv","index_or_path":2,"width":640,"height":480,"fps":30}}' \
   --teleop.type=so101_leader \
-  --teleop.port=/dev/ttyACM1 \
+  --teleop.port=/dev/ttyACM0 \
   --dataset.repo_id=AdrielP/cables_il_${SITUATION} \
   --dataset.root=./data/cables_il_${SITUATION} \
   --dataset.single_task="state based cable sorting: red yellow green targets, black white distractors" \
   --dataset.num_episodes=${NUM_EPISODES} \
   --dataset.episode_time_s=30 \
-  --dataset.reset_time_s=10 \
+  --dataset.reset_time_s=15 \
   --dataset.fps=30 \
   --dataset.push_to_hub=true \
   --play_sounds=false
